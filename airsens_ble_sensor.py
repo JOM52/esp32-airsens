346918c6d31db4bc025e733a5b884081f41e9bec
josmet52 <joseph.metrailler@bluewin.ch>
2022-01-09 18:03:24 +0100

    with machine.reset() on errors

--- airsens_ble_sensor.py
+++ airsens_ble_sensor.py
@@ -4,7 +4,7 @@
 """
 file: airsens_ble_sensor.py 
 
-version: 1.0
+version: 1.1
 date: 7.1.2022
 author: jom52
 
@@ -15,6 +15,8 @@ The sensors are made with an ESP32 microcontroller and can be powered by battery
 They transmit the data to a central also realized with an ESP32 by a bluetooth
 low energy communication (BLE)
 
+v1.0 : 7.1.2022 --> first prototype
+v1.1 : 9.1.2022 --> 
 """
 
 import ubluetooth
@@ -28,35 +30,53 @@ from lib.blink import blink_internal_blue_led
 from lib.ble_advertising import decode_services, decode_name
 from micropython import const
 
-# Choose the type of sensor connected
-BME_280_680 = 0
-if BME_280_680 == 280:
-    import lib.bme280 as bmex80
-elif BME_280_680 == 680:
-    import lib.bme680 as bmex80
-# else:
-#     print('ERROR\nNo know sensor defined. Correct that and restart the program')
-#     print('Possibilities are 280 or 680')
-#     print()
-#     print('push <ENTER> to exit')
-#     input()
-#     sys.exit()
-    
-# sensor pins and init
-BM_VCC_PIN = 15
-# BM_GND_PIN = 16
-BM_SDA_PIN = 21
-BM_SCL_pin = 22
-BM_VCC_PIN = machine.Pin(BM_VCC_PIN, machine.Pin.OUT)
-# BM_GND_PIN = machine.Pin(BM_GND_PIN, machine.Pin.OUT)
-BM_VCC_PIN.on()
-# BM_GND_PIN.off()
+# Hardware choices
+CONNECTED_SENSOR_TYPE = 'BME280' # 'NO_SENSOR' / 'BME280' / 'BME680'
+MICROCONTROLER = "TTGO" # 'TTGO' for ESP32 TTGO T-Display / WEMOS for ESP32 WEMOS D1 MINI
 
-# Constants
-T_DEEPSLEEP_MS = 10000
-T_BEFORE_DEEPSLEEP_MS = 100
-T_BETWEEN_2_DATA = 50
-T_WAIT_FOR_IRQ_TERMINATED_MS = 250
+if CONNECTED_SENSOR_TYPE == 'BME280':
+    import lib.bme280 as bmex80
+elif CONNECTED_SENSOR_TYPE == 'BME680':
+    import lib.bme680 as bmex80
+elif CONNECTED_SENSOR_TYPE == 'NO_SENSOR':
+    pass
+else:
+    print('ERROR\nNo known sensor defined. Correct that and restart the program')
+    print('Possibilities are 0, BME280 or BME680')
+    print()
+    print('push <ENTER> to exit')
+    input()
+    sys.exit()
+
+# sensor pins and init
+if MICROCONTROLER == 'TTGO':
+    BM_VCC_PIN = 15
+    BM_SDA_PIN = 21
+    BM_SCL_pin = 22
+    BM_VCC_PIN = machine.Pin(BM_VCC_PIN, machine.Pin.OUT)
+    BM_VCC_PIN.on()
+elif MICROCONTROLER == 'WEMOS':
+    BM_VCC_PIN = 15
+    BM_GND_PIN = 16
+    BM_SDA_PIN = 21
+    BM_SCL_pin = 22
+    BM_VCC_PIN = machine.Pin(BM_VCC_PIN, machine.Pin.OUT)
+    BM_GND_PIN = machine.Pin(BM_GND_PIN, machine.Pin.OUT)
+    BM_VCC_PIN.on()
+    BM_GND_PIN.off()
+else:
+    print('ERROR\nNo known microcontroler defined. Correct that and restart the program')
+    print('Possibilities are TTGO or WEMOS')
+    print()
+    print('push <ENTER> to exit')
+    input()
+    sys.exit()
+
+# Time constants
+T_DEEPSLEEP_MS = 30000 # interval between two measures
+T_BEFORE_DEEPSLEEP_MS = 50 # a short break before to go in deepsleep
+T_BETWEEN_2_DATA = 50 # intervall between two write on the bluetooth
+T_WAIT_FOR_IRQ_TERMINATED_MS = 10 # a short break when waititn to reduce power consumtion
 
 # analog voltage measurement
 R1 = 100e3 # first divider bridge resistor
@@ -71,8 +91,6 @@ ubatt.width(machine.ADC.WIDTH_12BIT)
 ubatt.atten(machine.ADC.ATTN_6DB)
 
 # IRQ constants
-# _IRQ_SCAN_RESULT = const(5)
-# _IRQ_SCAN_DONE = const(6)
 _IRQ_PERIPHERAL_CONNECT = const(7)
 _IRQ_PERIPHERAL_DISCONNECT = const(8)
 _IRQ_GATTC_SERVICE_RESULT = const(9)
@@ -80,16 +98,6 @@ _IRQ_GATTC_SERVICE_DONE = const(10)
 _IRQ_GATTC_CHARACTERISTIC_RESULT = const(11)
 _IRQ_GATTC_CHARACTERISTIC_DONE = const(12)
 
-# _ADV_IND = const(0x00)
-# _ADV_DIRECT_IND = const(0x01)
-# _ADV_SCAN_IND = const(0x02)
-# _ADV_NONCONN_IND = const(0x03)
-# 
-# _UART_SERVICE_UUID = ubluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
-# _UART_RX_CHAR_UUID = ubluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
-# _UART_TX_CHAR_UUID = ubluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
-
-#==== under test begin ==================================================================
 NUS_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
 RX_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
 TX_UUID = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
@@ -99,7 +107,6 @@ _UART_RX_CHAR_UUID = (ubluetooth.UUID(RX_UUID), ubluetooth.FLAG_WRITE)
 _UART_TX_CHAR_UUID = (ubluetooth.UUID(TX_UUID), ubluetooth.FLAG_READ | ubluetooth.FLAG_NOTIFY)
     
 BLE_UART_SERVICES = ((_UART_SERVICE_UUID, (_UART_TX_CHAR_UUID, _UART_RX_CHAR_UUID,)),)
-#==== under test end ==================================================================
 
 class BleJmbSensor:
     def __init__(self, ble):
@@ -116,7 +123,6 @@ class BleJmbSensor:
 
     def _reset(self):
         # Cached name and address from a successful scan.
-#         print('entering reset')
         self._name = None
         self._addr_type = None
         self._addr = None
@@ -150,31 +156,25 @@ class BleJmbSensor:
                 self._conn_handle = conn_handle
                 self._ble.gattc_discover_services(self._conn_handle)
                 self._irq_peripheral_connect = True
-#                 print('\n_irq_peripheral_connect')
 
         elif event == _IRQ_PERIPHERAL_DISCONNECT: #8
             # Disconnect (either initiated by us or the remote end).
             conn_handle, _, _ = data
-#             print()
-#             print('conn_handle', conn_handle,
-#                   '- self._conn_handle', self._conn_handle,
-#                   '- conn_handle == self._conn_handle --->', conn_handle == self._conn_handle)
+            print('conn_handle:', conn_handle, ' - self._conn_handle:', self._conn_handle)
             if conn_handle == self._conn_handle:
-                # If it was initiated by us, it'll already be reset.
+                # A system error has occurred. We reboot the microcontroller
                 machine.reset()
+            elif conn_handle == 65535:
+#                 print('Central is not running. Start it and restart this programm')
+                sys.exit('ERROR:\nCentral is not running. Start it and restart this programm')
             self._irq_peripheral_disconnect = True
 
         elif event == _IRQ_GATTC_SERVICE_RESULT: #9
             # Connected device returned a service.
-            
             while not self._irq_peripheral_connect:
                 utime.sleep_ms(T_WAIT_FOR_IRQ_TERMINATED_MS)
                 print('waiting for _irq_peripheral_connect')
-                
             conn_handle, start_handle, end_handle, uuid = data
-#             print('conn_handle, start_handle, end_handle, uuid', conn_handle, start_handle, end_handle, uuid)
-#             print('conn_handle == self._conn_handle', conn_handle == self._conn_handle)
-#             print('uuid == _UART_SERVICE_UUID', uuid == _UART_SERVICE_UUID)
             if conn_handle == self._conn_handle and uuid == _UART_SERVICE_UUID:
                 self._start_handle, self._end_handle = start_handle, end_handle
                 self._gattc_service_result = True
@@ -202,7 +202,6 @@ class BleJmbSensor:
             while not self._gattc_service_done:
                 utime.sleep_ms(T_WAIT_FOR_IRQ_TERMINATED_MS)
                 print('waiting for _gattc_service_done')
-                
             conn_handle, def_handle, value_handle, properties, uuid = data
             if conn_handle == self._conn_handle and uuid == _UART_RX_CHAR_UUID[0]:
                 self._rx_handle = value_handle
@@ -215,7 +214,6 @@ class BleJmbSensor:
             while not self._gattc_characteristic_result:
                 utime.sleep_ms(T_WAIT_FOR_IRQ_TERMINATED_MS)
                 print('waiting for _gattc_characteristic_result')
-                
             if self._tx_handle is not None and self._rx_handle is not None:
                 self._gattc_characteristic_done = True
             else:
@@ -258,9 +256,7 @@ class BleJmbSensor:
             self._ble.gap_connect(None)
             print('-------> self._ble.gap_connect(None)')
             return False
-#         t_start = utime.ticks_ms()
         self._ble.gap_connect(self._addr_type, self._addr)
-#         print('connect duration:' + str(utime.ticks_ms() - t_start) + 'ms')
         return True
 
     # Disconnect from current device.
@@ -284,23 +280,23 @@ def restart_ESP32(i, err_msg):
 
 
 def main():
-#     try:
-                
+    t_start_total = utime.ticks_ms()
+    try:
         print('initializing bluetooth')
         print('----------------------')
         # instanciation of bme280, bmex80 - Pin assignment
         i2c = machine.SoftI2C(scl=machine.Pin(BM_SCL_pin), sda=machine.Pin(BM_SDA_PIN), freq=10000)
         try:
-            if BME_280_680 == 280:
+            if CONNECTED_SENSOR_TYPE == 'BME280':
                 bmeX = bmex80.BME280(i2c=i2c)
-            elif BME_280_680 == 680:
+            elif CONNECTED_SENSOR_TYPE == 'BME680':
                 bmeX = bmex80.BME680_I2C(i2c=i2c)
-            elif BME_280_680 == 0:
+            elif CONNECTED_SENSOR_TYPE == 'NO_SENSOR':
                 bmeX = None
         except:
-            if BME_280_680 == 280:
+            if CONNECTED_SENSOR_TYPE == 'BME280':
                 sx = 'bme280'
-            elif BME_280_680 == 280:
+            elif CONNECTED_SENSOR_TYPE == 'BME680':
                 sx ='bme680'
             print('Ce n\'est pas un ' + sx + ' qui est branché?')
             print('Corrigez et relancez le programme!')
@@ -334,99 +330,96 @@ def main():
         addr = sensor._addr
 
         # main loop
-        while True:
-            # mesure time for a single pass
-            t_start_total = utime.ticks_ms()
-            # blink the blue led
-            blink_internal_blue_led(t_on_ms=100, t_off_ms=100, t_pause_ms=2, n_repeat=1)
-            # load the pass counter value from file
-            try:
-                with open ('index.txt', 'r') as f:
-                    i = int(f.readline()) + 1
-            except:
-                i = 1
-            with open ('index.txt', 'w') as f:
-                f.write(str(i))
+#         while True:
             
-            #connect to the central
-            sensor._addr_type, sensor._addr = addr_type, addr
-            print('connecting')
+        # mesure time for a single pass
+        t_start_passe = utime.ticks_ms()
+        # blink the blue led
+        blink_internal_blue_led(t_on_ms=100, t_off_ms=100, t_pause_ms=2, n_repeat=1)
+        # load the pass counter value from file
+        try:
+            with open ('index.txt', 'r') as f:
+                i = int(f.readline()) + 1
+        except:
+            i = 1
+        with open ('index.txt', 'w') as f:
+            f.write(str(i))
+        
+        #connect to the central
+        sensor._addr_type, sensor._addr = addr_type, addr
+        print('connecting')
+        connect_status = sensor.connect(sensor._addr_type, sensor._addr)
+        while not connect_status and not sensor._gattc_service_timeout:
+            print('----> waiting for connection --> timeaout status =', sensor._gattc_service_timeout)
+            utime.sleep_ms(T_WAIT_FOR_IRQ_TERMINATED_MS)
             connect_status = sensor.connect(sensor._addr_type, sensor._addr)
-            while not connect_status and not sensor._gattc_service_timeout:
-                print('----> waiting for connection --> timeaout status =', sensor._gattc_service_timeout)
-                utime.sleep_ms(T_WAIT_FOR_IRQ_TERMINATED_MS)
-                connect_status = sensor.connect(sensor._addr_type, sensor._addr)
-            
-#             if sensor._gattc_service_timeout:
-#                 print('----> sorti de la mauvaise connection')
-            # be sure that all task are terminated before to continue
-            while (not sensor.is_connected()
-                   or not sensor._gattc_service_done
-                   or not sensor._gattc_characteristic_done):
-                utime.sleep_ms(T_WAIT_FOR_IRQ_TERMINATED_MS)
-            # prepare the data's
-            msg_template = 'jmb'
-            if BME_280_680 == 0:
-                temp = [msg_template, 'temp', str(20)]
-                hum = [msg_template, 'hum', str(50)]
-                pres = [msg_template, 'pres', str(950)]
-                alt = [msg_template, 'alt', str(750)]
-                bat = [msg_template, 'bat', str(4)]
+        
+        # be sure that all task are terminated before to continue
+        while (not sensor.is_connected()
+               or not sensor._gattc_service_done
+               or not sensor._gattc_characteristic_done):
+            utime.sleep_ms(T_WAIT_FOR_IRQ_TERMINATED_MS)
+        # prepare the data's
+        msg_template = 'jmb'
+        if CONNECTED_SENSOR_TYPE == 'NO_SENSOR':
+            temp = [msg_template, 'temp', str(20)]
+            hum = [msg_template, 'hum', str(50)]
+            pres = [msg_template, 'pres', str(950)]
+            alt = [msg_template, 'alt', str(750)]
+            bat = [msg_template, 'bat', str(4)]
+            data_all = [temp, hum, pres, alt, bat]
+        else:            
+            temp = [msg_template, 'temp', str(bmeX.temperature)]
+            hum = [msg_template, 'hum', str(bmeX.humidity)]
+            pres = [msg_template, 'pres', str(bmeX.pressure)]
+            alt = [msg_template, 'alt', str(bmeX.altitude)]
+            bat = [msg_template, 'bat', str(ubatt.voltage / 1000)]
+            if CONNECTED_SENSOR_TYPE == 'BME680':
+                gas = [msg_template, 'gas', str(bmeX.gas / 1000)]
+                data_all = [temp, hum, pres, gas, alt, bat]
+            else:
                 data_all = [temp, hum, pres, alt, bat]
-            else:            
-                temp = [msg_template, 'temp', str(bmeX.temperature)]
-                hum = [msg_template, 'hum', str(bmeX.humidity)]
-                pres = [msg_template, 'pres', str(bmeX.pressure)]
-                alt = [msg_template, 'alt', str(bmeX.altitude)]
-                bat = [msg_template, 'bat', str(ubatt.voltage / 1000)]
-                if BME_280_680 == 680:
-                    gas = [msg_template, 'gas', str(bmeX.gas / 1000)]
-                    data_all = [temp, hum, pres, gas, alt, bat]
-                else:
-                    data_all = [temp, hum, pres, alt, bat]
-            print()
-            print()
-            # transmit the data to the central
-            for m in data_all:
-                msg = " ".join(m)
-                sensor.write(msg)
-                print(len(msg), msg)
-                utime.sleep_ms(T_BETWEEN_2_DATA)
-            sensor.write('jmb\n')
-            # disconnect from the central
-            sensor.disconnect()
-            while not sensor._irq_peripheral_disconnect:
-                utime.sleep_ms(T_WAIT_FOR_IRQ_TERMINATED_MS)
-            # last tasks
-            elapsed = utime.ticks_ms() - t_start_total
-            print()
-            print('pass:', i, '-->',  str((utime.ticks_ms() - t_start_total)/1000) + 's', )
-            print('going to sleep for ' + str(int((T_BEFORE_DEEPSLEEP_MS + T_DEEPSLEEP_MS - elapsed)/1000)) + 's')
+        print()
+        # transmit the data to the central
+        for m in data_all:
+            msg = " ".join(m)
+            sensor.write(msg)
+            print(len(msg), msg)
+            utime.sleep_ms(T_BETWEEN_2_DATA)
+        sensor.write('jmb\n')
+        # disconnect from the central
+        sensor.disconnect()
+        while not sensor._irq_peripheral_disconnect:
+            utime.sleep_ms(T_WAIT_FOR_IRQ_TERMINATED_MS)
+        # last tasks
+        blink_internal_blue_led(t_on_ms=200, t_off_ms=100, t_pause_ms=2, n_repeat=2)
+        elapsed = utime.ticks_ms() - t_start_passe
+        print()
+        print('pass:', i, '-->',  str((utime.ticks_ms() - t_start_total)/1000) + 's', )
+        print('going to sleep for ' + str(int((T_BEFORE_DEEPSLEEP_MS + T_DEEPSLEEP_MS - elapsed)/1000)) + 's')
                 
-#             print(sensor._irq_list)
-            sensor._irq_list = []
-            print()
-            print('======================')
-            blink_internal_blue_led(t_on_ms=200, t_off_ms=100, t_pause_ms=2, n_repeat=2)
-            utime.sleep_ms(T_BEFORE_DEEPSLEEP_MS + T_DEEPSLEEP_MS - elapsed)
+#         print(sensor._irq_list)
+        sensor._irq_list = []
+        print('======================')
+#         utime.sleep_ms(T_BEFORE_DEEPSLEEP_MS + T_DEEPSLEEP_MS - elapsed)
         
-#         print('going to deepsleep for: ' + str(int((T_BEFORE_DEEPSLEEP_MS + T_DEEPSLEEP_MS - elapsed)/1000)) + 's')
-#         utime.sleep_ms(T_BEFORE_DEEPSLEEP_MS)
-#         machine.deepsleep(T_DEEPSLEEP_MS - elapsed)
+        print('going to deepsleep for: ' + str(int((T_BEFORE_DEEPSLEEP_MS + T_DEEPSLEEP_MS - elapsed)/1000)) + 's')
+        utime.sleep_ms(T_BEFORE_DEEPSLEEP_MS)
+        machine.deepsleep(T_DEEPSLEEP_MS - elapsed)
         
-#     except:
-#         try:
-#             with open ('index.txt', 'r') as f:
-#                 i = int(f.readline())
-#         except:
-#             i = 1
-# #         restart_ESP32(i, msg)
-#         msg = str(i) + ' - restart_ESP32: ' 
-#         print(msg)
-#         with open('error.txt' , 'a') as f:
-#             f.write(msg+'\n')
-#         utime.sleep_ms(10000)
-#         machine.reset()
+    except:
+        try:
+            with open ('index.txt', 'r') as f:
+                i = int(f.readline())
+        except:
+            i = 1
+        restart_ESP32(i, msg)
+        msg = str(i) + ' - restart_ESP32: ' 
+        print(msg)
+        with open('error.txt' , 'a') as f:
+            f.write(msg+'\n')
+        utime.sleep_ms(2000)
+        machine.reset()
         
 
 if __name__ == "__main__":
