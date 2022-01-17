ea75581dbd46df17402f1c1d8c66b79344b110b3
josmet52 <joseph.metrailler@bluewin.ch>
2022-01-17 22:51:28 +0100

    added wifi and mqtt in central

--- airsens_ble_central.py
+++ airsens_ble_central.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-file: airsens_central.py 
+file: airsens_central.py
 
 author: jom52
 email: jom52.dev@gmail.com
@@ -15,11 +15,18 @@ v1.0 : 07.01.2022 --> first prototype
 v1.2 : 17.01.2022 -->cleaned up, prototype stable for long test
 """
 
+import ubluetooth
+import ubinascii
+import machine
+import utime
+import lib.umqttsimple2_jo as umqttsimple
+
+from lib.wifi_esp32 import WifiEsp32 
+from lib.rtc_esp32 import RtcEsp32
 from machine import Pin, Timer, SoftI2C
 from utime import sleep_ms
-import ubluetooth
 
-CENTRAL_NAME = "jmb_airsens_ttgo_01"
+CENTRAL_NAME = "jmb_airsens_wemos_01"
 
 class BLE():
     def __init__(self, name):   
@@ -37,7 +44,10 @@ class BLE():
         self.ble.irq(self.ble_irq)
         self.register()
         self.advertiser()
-
+        
+        self.client = None
+        self.TOPIC_PUB = None
+        
     # the blue led stop blinking wenn disconnected
     def disconnected(self):        
         self.timer1.deinit()
@@ -68,7 +78,11 @@ class BLE():
             if message[:3] == 'jmb':
                 data = message.split(' ')
                 if len(data) > 1:
-                    print(data[1], data[2])
+                    msg = str(data[1]) + '-' + str(data[2])
+                    print(msg)
+            #         msg += 'MQTT client connected on ' + MQTT_BROKER + ' with the topic ' + TOPIC_PUB
+                    self.client.publish(self.TOPIC_PUB, msg)
+#                     utime.sleep_ms(50)
                 else:
                     self.pass_counter += 1
                     print('passe:', self.pass_counter)
@@ -97,11 +111,37 @@ class BLE():
     def advertiser(self):
         name = bytes(self.name, 'UTF-8')
         self.ble.gap_advertise(100, bytearray('\x02\x01\x02') + bytearray((len(name) + 1, 0x09)) + name)
+        
+def connect_and_subscribe(client_id, mqtt_broker, topic_pub):
+    client = umqttsimple.MQTTClient(client_id, mqtt_broker)
+    client.connect()
+#     print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_broker, topic_pub))
+    return client
   
 def main():
+    
+    MQTT_BROKER = '192.168.1.108'
+    CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+    TOPIC_PUB = 'airsens_test'
+    
     print('central listening as <' + CENTRAL_NAME + '>')
+    print('-----------------------------------------------------------')
+    
+    my_wifi = WifiEsp32('jmb-home', 'lu-mba01')  # initialize the class
+    my_wifi.connect_wifi()  # connect to the wifi network
+    
+    my_rtc = RtcEsp32()  # initialize the class
+    my_rtc.rtc_init()  # initialize the rtc with local date and time
+    # extract the date and time values in str format and print it
+    now = my_rtc.rtc_now()  # get date and time
+    datetime_formated = my_rtc.format_datetime(now)
+    print("now date and time :", datetime_formated)
+    print('-----------------------------------------------------------')
+    
     blue_led = Pin(2, Pin.OUT)
     ble = BLE(CENTRAL_NAME)
+    ble.TOPIC_PUB = TOPIC_PUB
+    ble.client = connect_and_subscribe(CLIENT_ID, MQTT_BROKER, TOPIC_PUB)
 
 if __name__ == '__main__':
     main()
